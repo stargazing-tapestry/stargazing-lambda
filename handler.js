4e1451bf07db667332d6011f306b252beb5388ce
jerry.zhang <jerry.zhang@rea-group.com>
2018-08-16 10:27:08 +1000

    Add parsing logic

--- handler.js
+++ handler.js
@@ -2,23 +2,57 @@
 const AWS = require('aws-sdk');
 const mapping = require('mapping_data');
 
+
 module.exports.starGaze = (event, context, callback) => {
-    const iotData = new AWS.IotData({endpoint: process.env.AWS_IOT_ENDPOINT});
-    const params = {
-        topic: 'test_topic',
-        payload: '{"test": "test"}'
+    const acceptedResp = {
+        statusCode: 201
     };
 
-    iotData.publish(params, (err, res) => {
+    const responseCallback = (err, res) => {
         if (err) return context.fail(err);
+        callback(null, acceptedResp);
+    };
 
-
-        const response = {
-            statusCode: 201
+    const publishMessage = function (payload) {
+        const iotData = new AWS.IotData({endpoint: process.env.AWS_IOT_ENDPOINT});
+        const params = {
+            topic: 'test_topic',
+            payload: payload
         };
 
-        callback(null, response);
-    });
+        iotData.publish(params, responseCallback);
+    };
+
+    const parseInput = function (stringInput) {
+        try {
+            return JSON.parse(stringInput);
+        } catch (e) {
+            return null
+
+        }
+    };
+
+    const input = parseInput(event.body)
+
+    if (input) {
+        const constellation = input['constellation']
+
+        if (constellation) {
+            const payload = mapping[constellation.toLowerCase()]
+            if (payload) {
+                console.log("found mapping" , payload)
+                publishMessage(JSON.stringify(payload));
+            } else {
+                console.log("couldn't find mapping" , constellation)
+                callback(null, acceptedResp)
+            }
+        } else {
+            console.log("couldn't find constellation input", event.body)
+            callback(null, acceptedResp)
+        }
+    } else {
+        callback(null, {statusCode: 400})
+    }
 
 };
 
