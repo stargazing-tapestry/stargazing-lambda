775ce1fdeac121a2beed89eb72dfa34a25a77f5c
jerry.zhang <jerry.zhang@rea-group.com>
2018-08-16 13:36:27 +1000

    Make it send binary to mqtt

--- handler.js
+++ handler.js
@@ -32,16 +32,31 @@ module.exports.starGaze = (event, context, callback) => {
         }
     };
 
+    //Convert colour from html to rgb binanry
+    const convertColour = function (htmlColour) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(htmlColour);
+        return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
+    }
+
     const input = parseInput(event.body)
 
     if (input) {
         const constellation = input['constellation']
+        const colour = input['colour']
 
-        if (constellation) {
+        if (constellation && colour) {
             const payload = mapping[constellation.toLowerCase()]
             if (payload) {
+                let result = []
+                let binaryColour = convertColour(colour)
+                let binaryArray = payload.forEach(i => {
+                    result = result.concat(binaryColour)
+                    result.push((i >> 8) & 0xff)
+                    result.push(i & 0xff)
+                });
+                console.log(result, binaryColour)
                 console.log("found mapping" , payload)
-                publishMessage(JSON.stringify(payload));
+                publishMessage(Buffer.from(result))
             } else {
                 console.log("couldn't find mapping" , constellation)
                 callback(null, acceptedResp)
